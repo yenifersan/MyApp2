5bf664a1168b79d46e5552849a396dc774f500f4
yenifersan <yenifer.santiago@tecsup.edu.pe>
2019-06-21 13:32:37 -0500

    comit

---
+++ app/src/main/java/com/example/myapp/activities/DetailVentaActivity.java
@@ -0,0 +1,18 @@
+package com.example.myapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myapp.R;
+
+
+public class DetailVentaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_venta);
+    }
+
+}
---
+++ app/src/main/java/com/example/myapp/activities/ListVentaActivity.java
@@ -0,0 +1,16 @@
+package com.example.myapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myapp.R;
+
+public class ListVentaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_venta);
+    }
+}
